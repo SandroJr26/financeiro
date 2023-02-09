f3054ef714ac8b6030df8488e659d31f614846f5
Sandro Junior <nunet26@gmail.com>
2023-02-09 00:53:11 -0300

    teste js

--- script/script.js
+++ script/script.js
@@ -4,10 +4,15 @@ let saldoAtual = document.querySelector(".saldoAtual span").innerHTML;
 function adicionarEntrada() {
   let valorEntrada = document.getElementById("entrada").value;
   let totalEntrada = document.querySelector(".js-entrada .entrada").innerHTML;
-  let valorEntradaTotal = parseFloat(valorEntrada) + parseFloat(totalEntrada);
+  let valorEntradaTotal =
+    Number(valorEntrada.replace(",", ".")) +
+    Number(totalEntrada.replace(",", "."));
   let valorFormatado = valorEntradaTotal.toLocaleString("pt-BR", {
     minimumFractionDigits: 2,
   });
   document.querySelector(".js-entrada .entrada").innerHTML = valorFormatado;
+  console.log("valorEntrada", valorEntrada);
+  console.log("totalEntrada", totalEntrada);
+  console.log("valorEntradaTotal", valorEntradaTotal);
+  console.log("valorFormatado", valorFormatado);
 }
-1;
