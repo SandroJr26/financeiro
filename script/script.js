76925c4235d48c3cdfc849aee8ce7c69388968cc
Sandro Junior <nunet26@gmail.com>
2023-02-10 16:01:48 -0300

    js soma de funcoes

--- script/script.js
+++ script/script.js
@@ -1,18 +1,48 @@
-let saida = document.querySelector(".js-saida .saida").innerHTML;
-let saldoAtual = document.querySelector(".saldoAtual span").innerHTML;
+// let saida = document.querySelector(".js-saida .saida").innerHTML;
+// let saldoAtual = document.querySelector(".saldoAtual span").innerHTML;
+
+// function adicionarEntrada() {
+//   let valorEntrada = document.getElementById("entrada").value;
+//   let totalEntrada = document.querySelector(".js-entrada .entrada").innerHTML;
+//   let valorEntradaTotal =
+//     Number(valorEntrada.replace(",", ".")) +
+//     Number(totalEntrada.replace(",", "."));
+//   let valorFormatado = valorEntradaTotal.toLocaleString("pt-BR", {
+//     minimumFractionDigits: 2,
+//   });
+//   document.querySelector(".js-entrada .entrada").innerHTML = valorFormatado;
+//   console.log("valorEntrada", valorEntrada);
+//   console.log("totalEntrada", totalEntrada);
+//   console.log("valorEntradaTotal", valorEntradaTotal);
+//   console.log("valorFormatado", valorFormatado);
+// }
+
+let totalEntrada = 0;
+let totalSaida = 0;
+let totalSaldo = 0;
 
 function adicionarEntrada() {
-  let valorEntrada = document.getElementById("entrada").value;
-  let totalEntrada = document.querySelector(".js-entrada .entrada").innerHTML;
-  let valorEntradaTotal =
-    Number(valorEntrada.replace(",", ".")) +
-    Number(totalEntrada.replace(",", "."));
-  let valorFormatado = valorEntradaTotal.toLocaleString("pt-BR", {
-    minimumFractionDigits: 2,
+  const valorEntrada = document.getElementById("entrada").value;
+  totalEntrada += Number(valorEntrada);
+  valorEntradaFormatado = totalEntrada.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
   });
-  document.querySelector(".js-entrada .entrada").innerHTML = valorFormatado;
-  console.log("valorEntrada", valorEntrada);
-  console.log("totalEntrada", totalEntrada);
-  console.log("valorEntradaTotal", valorEntradaTotal);
-  console.log("valorFormatado", valorFormatado);
+  document.querySelector(".js-entrada .entrada").innerHTML =
+    valorEntradaFormatado;
+  return valorEntradaFormatado;
 }
+
+function adicionarSaida() {
+  const valorSaida = document.getElementById("saida").value;
+  totalSaida -= Number(valorSaida);
+  valorSaidaFormatado = totalSaida.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+  document.querySelector(".js-saida .saida").innerHTML = valorSaidaFormatado;
+  return valorSaidaFormatado;
+}
+
+adicionarEntrada();
+adicionarSaida();
